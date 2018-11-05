4482f8a1b09d257b289d3caf593b419699ed8ef6
fguezout <fraoucene.guezout.externe@chronopost.fr>
2018-11-05 16:25:49 +0100

    Test integration

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
+   
 app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
